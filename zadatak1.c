12bf81e2311f9e0a949fda2f3ee8a663144a0902
mjuric05 <92426494+mjuric05@users.noreply.github.com>
2021-11-02 15:35:24 +0100

    Update zadatak1.c

--- zadatak1.c
+++ zadatak1.c
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,27 +14,27 @@ typedef struct _student
     double bodovi;
 }student;
 
-int procitajBrojRedakaIzDatoteke(char* nazivDatoteke);          //funkcija vraca broj redaka tj. studenata
-student* alocirajMemoriju(int brojSutdenata,char* nazivDatoteke);   //funkcija koja ce sa brojem studenata zauzeti potrebnu memoriju
-void ispisNaEkran(char*nazivDatoteke,int brojSutdenata);        //funkcija za ispis studenata na standardni izlaz tj. ekran            
+int procitajBrojRedakaIzDatoteke(char* nazivDatoteke);                  //funkcija vraca broj redaka tj. studenata
+student* alocirajMemoriju(int brojSutdenata, char* nazivDatoteke);      //funkcija koja ce sa brojem studenata zauzeti potrebnu memoriju
+void ispisNaEkran(char* nazivDatoteke, int brojSutdenata);              //funkcija za ispis studenata na standardni izlaz tj. ekran            
 
 int main()
 {
-    int i=0,brRedaka=0;
-    FILE* dat=NULL;
+    int i = 0, brRedaka = 0;
+    FILE* dat = NULL;
     student* s;
     char* nazivDatoteke = NULL;
-    
-    printf("Unesite ime datoteke:\n");
-    scanmf(" %s",&nazivDatoteke);
-    
-    dat=fopen(nazivDatoteke,"r");
-    
-    brRedaka=procitajBrojRedakaIzDatoteke(nazivDatoteke);
-    s=alocirajMemoriju(brRedaka,nazivDatoteke);
 
-    ispisNaEkran(nazivDatoteke,brRedaka);    
-    
+    printf("Unesite ime datoteke:\n");
+    scanf(" %s", nazivDatoteke);
+
+    dat = fopen(nazivDatoteke, "r");
+
+    brRedaka = procitajBrojRedakaIzDatoteke(nazivDatoteke);
+    s = alocirajMemoriju(brRedaka, nazivDatoteke);
+
+    ispisNaEkran(nazivDatoteke, brRedaka);
+
     fclose(dat);
 
     return 0;
@@ -40,88 +42,88 @@ int main()
 
 int procitajBrojRedakaIzDatoteke(char* nazivDatoteke)
 {
-    int brojac=0;
-    FILE* dat=NULL;
-    char buffer[MAX_LINE]={0};
-    
-    dat=fopen(nazivDatoteke,"r");
-    
-    if(!dat)
+    int brojac = 0;
+    FILE* dat = NULL;
+    char buffer[MAX_LINE] = { 0 };
+
+    dat = fopen(nazivDatoteke, "r");
+
+    if (!dat)
     {
         printf("Greska pri otvaranju Vase datoteke!\n");
         return -1;
     }
-    while(!feof(dat)) 
+    while (!feof(dat))
     {
-        fgets(buffer,MAX_LINE,dat);
+        fgets(buffer, MAX_LINE, dat);
         brojac++;
     }
-    
+
     fclose(dat);
-    
+
     return brojac;
 }
 
-student* alocirajMemoriju(int brojSutdenata,char* nazivDatoteke)
+student* alocirajMemoriju(int brojSutdenata, char* nazivDatoteke)
 {
-    int brojac=0;
-    FILE* dat=NULL;
-    student* studenti=NULL;
+    int brojac = 0;
+    FILE* dat = NULL;
+    student* studenti = NULL;
 
-    studenti=(student*)malloc(brojSutdenata*sizeof(student));
-    
-    dat=fopen(nazivDatoteke,"r");
-    
-    if(!dat)
+    studenti = (student*)malloc(brojSutdenata * sizeof(student));
+
+    dat = fopen(nazivDatoteke, "r");
+
+    if (!dat)
     {
         printf("Greska prilikom alociranja memeorije!\n");
         free(studenti);
         return NULL;
     }
-    while(!feof(dat))
+    while (!feof(dat))
     {
-        fscanf(dat," %s %s %lf",studenti[brojac].ime,studenti[brojac].prezime,&studenti[brojac].bodovi);
+        fscanf(dat, " %s %s %lf", studenti[brojac].ime, studenti[brojac].prezime, &studenti[brojac].bodovi);
         brojac++;
     }
-    
+
     fclose(dat);
-    
+
     return studenti;
 }
 
-void ispisNaEkran(char*nazivDatoteke,int brojSutdenata)
+void ispisNaEkran(char* nazivDatoteke, int brojSutdenata)
 {
-    int i=0;
-    FILE* dat=NULL;
+    int i = 0;
+    FILE* dat = NULL;
     student* s;
-    double max=0.0;
-    
-    s=(student*)malloc(brojSutdenata*sizeof(student));
-    
-    dat=fopen(nazivDatoteke,"r");
-     if(!dat)
+    double max = 0.0;
+
+    s = (student*)malloc(brojSutdenata * sizeof(student));
+
+    dat = fopen(nazivDatoteke, "r");
+    if (!dat)
     {
-        printf("Greska prilikom otcaranja!\n");
+        printf("Greska prilikom otvaranja!\n");
     }
-    
-    for(i=0;i<brojSutdenata;i++)
+
+    for (i = 0; i < brojSutdenata; i++)
     {
-        fscanf(dat," %s %s %lf",s[i].ime,s[i].prezime,&s[i].bodovi);
+        fscanf(dat, " %s %s %lf", s[i].ime, s[i].prezime, &s[i].bodovi);
     }
-    
-    max=s[0].bodovi;
-    
-    for(i=1;i<brojSutdenata;i++)
+
+    max = s[0].bodovi;
+
+    for (i = 1; i < brojSutdenata; i++)
     {
-        if(max<s[i].bodovi)
-            max=s[i].bodovi;
+        if (max < s[i].bodovi)
+            max = s[i].bodovi;
     }
-    
-    for(i=0;i<brojSutdenata;i++)
+
+    for (i = 0; i < brojSutdenata; i++)
     {
         printf("Rezultati:\n");
-        printf("Ime: %s Prezime: %s Bodovi(apsloutni): %lf Bodovi(relativni): %lf\n",s[i].ime,s[i].prezime,s[i].bodovi,s[i].bodovi/max*100);
+        printf("Ime: %s Prezime: %s Bodovi(apsloutni): %lf Bodovi(relativni): %lf\n", s[i].ime, s[i].prezime, s[i].bodovi, s[i].bodovi / max * 100);
     }
-    
+
     fclose(dat);
 }
